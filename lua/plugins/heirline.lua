11033d4c5a178a116cb4291ae07d136c75b1c1ee
srdusr <graytrevor98@gmail.com>
2022-11-07 12:03:16 +0200

    fixed deprecated flexible component option

--- lua/plugins/heirline.lua
+++ lua/plugins/heirline.lua
@@ -603,30 +603,32 @@ local WordCount = {
 
 -- Working Directory
 local WorkDir = {
-	provider = function(self)
-		self.icon = (vim.fn.haslocaldir(0) == 1 and "l" or "g") .. " " .. " "
-		local cwd = vim.fn.getcwd(0)
-		self.cwd = vim.fn.fnamemodify(cwd, ":~")
-	end,
-	hl = { fg = colors.blue, bold = true, bg = colors.bg },
-
-	utils.make_flexible_component(1, {
-		-- evaluates to the full-lenth path
-		provider = function(self)
-			local trail = self.cwd:sub(-1) == "/" and "" or "/"
-			return self.icon .. self.cwd .. trail .. " "
-		end,
-	}, {
-		-- evaluates to the shortened path
-		provider = function(self)
-			local cwd = vim.fn.pathshorten(self.cwd)
-			local trail = self.cwd:sub(-1) == "/" and "" or "/"
-			return self.icon .. cwd .. trail .. " "
-		end,
-	}, {
-		-- evaluates to "", hiding the component
-		provider = "",
-	}),
+    init = function(self)
+        self.icon = (vim.fn.haslocaldir(0) == 1 and "l" or "g") .. " " .. " "
+        local cwd = vim.fn.getcwd(0)
+        self.cwd = vim.fn.fnamemodify(cwd, ":~")
+    end,
+    hl = { fg = "colors.blue", bold = true },
+    flexible = 1,
+    {
+        -- evaluates to the full-lenth path
+        provider = function(self)
+            local trail = self.cwd:sub(-1) == "/" and "" or "/"
+            return self.icon .. self.cwd .. trail .." "
+        end,
+    },
+    {
+        -- evaluates to the shortened path
+        provider = function(self)
+            local cwd = vim.fn.pathshorten(self.cwd)
+            local trail = self.cwd:sub(-1) == "/" and "" or "/"
+            return self.icon .. cwd .. trail .. " "
+        end,
+    },
+    {
+        -- evaluates to "", hiding the component
+        provider = "",
+    }
 }
 
 -- Snippets Indicator
