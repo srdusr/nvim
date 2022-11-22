c7e5dd4a2ab4eb9cfca9b7ac29e52a4d34803183
srdusr <graytrevor98@gmail.com>
2022-11-22 21:48:58 +0200

    Added 'iamcco/markdown-preview.nvim'

--- lua/user/pack.lua
+++ lua/user/pack.lua
@@ -390,6 +390,11 @@ return packer.startup(function(use)
 	--      [".md"] = "markdown",
 	--      [".markdown"] = "markdown",
 	--      [".mdown"] = "markdown",
+  use({
+    "iamcco/markdown-preview.nvim",
+    run = function() vim.fn["mkdp#util#install"]() end,
+  })
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
